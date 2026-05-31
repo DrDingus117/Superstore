f0192f2b4edfb2db9954dc102c7d113818f1d3e4
Marcus Pendleton <marcuspendleton100@gmail.com>
2026-05-31 16:47:13 -0700

    Implement error handling in GetAddressTypes procedure
    Added error handling to GetAddressTypes procedure.

--- Stored Procedures/GetAddressTypes.sql
+++ Stored Procedures/GetAddressTypes.sql
@@ -1,15 +1,41 @@
 USE [Superstore]
 GO
 
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:      Marcus Pendleton
+-- Create date: 5/31/2026
+-- Update date: 5/31/2026
+-- Description: Get Address Types
+--
+-- EXEC GetAddressTypes
+-- =============================================
+
 CREATE OR ALTER PROCEDURE dbo.GetAddressTypes
 AS
 BEGIN
     SET NOCOUNT ON;
 
-    SELECT
-        AddressTypeID,
-        AddressType
-    FROM dbo.AddressType
-    ORDER BY AddressType;
+    BEGIN TRY
+
+        SELECT
+            AddressTypeID,
+            AddressType
+        FROM dbo.AddressType
+        ORDER BY AddressType;
+
+    END TRY
+
+    BEGIN CATCH
+
+        SELECT
+            ERROR_MESSAGE() AS ErrorMessage;
+
+    END CATCH
 END
 GO
